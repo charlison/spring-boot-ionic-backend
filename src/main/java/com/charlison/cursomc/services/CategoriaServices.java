53e3bb2710dc883c35cc0312de7a2c1ffaf3e33b
Charlison Fideles <charlin.pg@hotmail.com>
2019-09-13 11:27:19 -0300

    Atualizando categoria com PUT, ajustes pequenos metodo buscar/find

--- src/main/java/com/charlison/cursomc/services/CategoriaServices.java
+++ src/main/java/com/charlison/cursomc/services/CategoriaServices.java
@@ -15,7 +15,7 @@ public class CategoriaServices {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 
 		Optional<Categoria> obj = repo.findById(id);
 		
@@ -24,7 +24,12 @@ public class CategoriaServices {
 	}
 	
 	public Categoria insert(Categoria obj) {
-		//obj.setId(null);
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
 		return repo.save(obj);
 	}
 }
